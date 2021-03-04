f575ef317333fa0fc2bc9851f60e957bdae46435
Renan Oliveira <orenan124@gmail.com>
2021-03-04 17:00:08 -0300

    setup

---
+++ src/main/java/com/renanoliveira/springbootjavarestapi/SpringbootjavarestapiApplication.java
@@ -0,0 +1,13 @@
+package com.renanoliveira.springbootjavarestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootjavarestapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootjavarestapiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/renanoliveira/springbootjavarestapi/SpringbootjavarestapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.renanoliveira.springbootjavarestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootjavarestapiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
